0146b4ab8cb2a4acdba6a9a67f7720898efd858d
Simon Chase <smchase@protonmail.com>
2023-01-15 22:58:24 -0500

    updates

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
 const app = express()
 const port = 3000
 
-app.use(bodyParser.json())
+app.use(json())
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+	urlencoded({
+		extended: true,
+	})
 )
 
 app.get('/', (request, response) => {
