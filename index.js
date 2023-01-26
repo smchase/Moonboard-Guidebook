7325b9a150fceaccd3b0958c27a07b81fbaba4e1
Simon Chase <smchase@protonmail.com>
2023-01-25 20:41:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('./queries');
 const app = express();
 const path = require('path');
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
