dfb897ba89fad645e45b84e66b450259b800669b
Simon Chase <smchase@protonmail.com>
2023-08-07 16:11:59 -0400

    unbuffer output

--- setup/update.sh
+++ setup/update.sh
@@ -2,7 +2,7 @@
 exec > log.txt 2>&1
 echo "Running update at $(date)"
 git pull
-python3 get_benchmarks.py
-python3 augment_benchmarks.py
-python3 process_benchmarks.py
-python3 update_db.py
+python3 -u get_benchmarks.py
+python3 -u augment_benchmarks.py
+python3 -u process_benchmarks.py
+python3 -u update_db.py
