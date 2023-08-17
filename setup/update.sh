78fc5a6931328c0d65eed60aee2d78b20654f095
Simon Chase <smchase@protonmail.com>
2023-08-17 00:31:06 -0400

    added proper logging

--- setup/update.sh
+++ setup/update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+log_dir="logs/$(date +\%Y/\%m/\%d)"
+log_file="$log_dir/log_$(date +\%Y-\%m-\%d_\%H:\%M).txt"
 exec > log.txt 2>&1
 echo "Running update at $(date)"
 git pull
@@ -7,3 +9,5 @@ python3 -u augment_benchmarks.py
 python3 -u process_benchmarks.py
 python3 -u update_db.py
 echo "Done!"
+mkdir -p "$log_dir"
+cp log.txt "$log_file"
